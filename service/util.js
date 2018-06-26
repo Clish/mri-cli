f8d1c3eb1162a11a5825df484e47658999e50985
Clish wang <wangmingzhe@admaster.com.cn>
2018-06-26 17:33:05 +0800

    add

--- service/util.js
+++ service/util.js
@@ -1,7 +1,19 @@
-const _ = require('lodash');
-const _fse = require('fs-extra');
-const _path = require('path');
-const _fs = require('fs');
+
+const _fse   = require('fs-extra');
+const _      = require('lodash');
+const _path  = require('path');
+const _fs    = require('fs');
+const moment = require('moment');
+const ejs    = require('ejs');
+
+const replaceTempVars = (temp, vars, value) => {
+    let obj = _.isObject(vars) ? vars : {[vars]: value || ''};
+    _.forEach(obj, (val, key) => {
+        let regex = new RegExp(_.escapeRegExp(`$_{${key}}`), 'ig');
+        temp = temp.replace(regex, val);
+    });
+    return temp;
+}
 
 module.exports = {
     ifnvl(src, target) {
@@ -22,5 +34,18 @@ module.exports = {
             }
         });
         return path;
+    },
+
+    createComments(options) {
+        let obj = { time: moment().format('YYYY/MM/DD HH:mm:ss') };
+        let commentFile = _fs.readFileSync(_path.join(__dirname, '../template/comments'), { encoding: 'utf8' });
+        let gitConfig = _fs.readFileSync(_path.join(process.cwd(), '.git/config'), { encoding: 'utf8' });
+        _.forEach(['name', 'email'], str => {
+            let  rexStr = `${str} = `;
+            let  regex  = new RegExp(rexStr + '.*');
+            obj[str] = regex.exec(gitConfig) ? regex.exec(gitConfig)[0].replace(rexStr, '') : '';
+        });
+
+        return ejs.render(commentFile, { ...options, ...obj });
     }
 };
