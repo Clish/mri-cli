d42ff29c5ea212e412232171c606b335d92e6872
amily4555 <amily4555@gmail.com>
2018-07-05 17:54:02 +0800

    添加模板

---
+++ template/project/env/social-dev-const.ts
@@ -0,0 +1,18 @@
+/**
+ * const 的补充
+ * 根据不同环境，值有所不同
+ */
+
+const envConst =  {
+
+    'ENV': 'dev',
+
+    // 是否打开debug模式
+    'DEBUG': true,
+
+    // 当前开发环境的 origin
+    'X-ORIGIN': 'dev.masterrt.mri',
+
+};
+
+export default envConst;
