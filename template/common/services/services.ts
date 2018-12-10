fb71f1826e8d7ef105c37a5905c39836e81d2782
amily4555 <amily4555@gmail.com>
2018-12-10 14:35:26 +0800

    add mri git conflict

--- template/common/services/services.ts
+++ template/common/services/services.ts
@@ -8,7 +8,7 @@
 import * as moment from 'moment';
 import * as _ from 'lodash';
 import {MrServices} from 'masterrt';
-import * as mu from 'mzmu';
+import mu from 'mzmu';
 import router from 'umi/router';
 import {$utils} from 'src/services';
 import {IBaseServices} from 'src/common/services';
--- template/project/theme/dva.ts
+++ template/project/theme/dva.ts
@@ -1,6 +1,5 @@
 /**
  * dva.ts
- * 项目启动，配置全局设置以及预加载
  * 其他详见 https://dvajs.com/guide/getting-started.html#connect-%E8%B5%B7%E6%9D%A5
  *
  * @author ...
@@ -8,21 +7,10 @@
 
 import $theme from 'src/theme';
 import { MrServices } from 'masterrt';
-import * as mu from 'mzmu';
+import mu from 'mzmu';
 import { $pool, $utils, $intl } from 'src/services';
 
 export default function config() {
-    let <%=upperName%>Const = $theme.getProviders('<%=upperName%>Const');
-
-    console.debug('init::-> dva <%=name%>');
-    
-    MrServices.setHeaders({
-        'X-TOKEN': () => mu.storage(<%=upperName%>Const.STORAGE_X_TOKEN) || '',
-        'X-LANG': () => mu.storage(<%=upperName%>Const.STORAGE_LOCALE) || <%=upperName%>Const.LOCALE,
-        'X-ORIGIN': <%=upperName%>Const['X-ORIGIN']
-    });
-
-    MrServices.setResourcePool($pool);
 
     return {
         onError(err) {
